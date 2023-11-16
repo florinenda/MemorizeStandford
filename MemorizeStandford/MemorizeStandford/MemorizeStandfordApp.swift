ad51267e97d93bd54b7b8ec5c0f14903c537d601
Amarildo Lucas <vmarildo@gmail.com>
2023-11-16 18:03:26 -0300

    first commit

---
+++ MemorizeStandford/MemorizeStandford/MemorizeStandfordApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeStandfordApp.swift
+//  MemorizeStandford
+//
+//  Created by Amarildo João Custódio Lucas on 16/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeStandfordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
